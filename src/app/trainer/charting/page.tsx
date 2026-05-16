10ca22a0e50efdbe75a70fbfa8957408a6b8fd1e
FassTLee <shotace@khu.ac.kr>
2026-05-16 16:28:09 +0900

    chore: 고아 라우트 3개 삭제 (charting, report, sequence)

--- src/app/trainer/charting/page.tsx
+++
@@ -1,4 +0,0 @@
-import { Phase2Disabled } from '@/components/common/Phase2Disabled'
-export default function ChartingPage() {
-  return <Phase2Disabled featureName="PT 세션 기록" />
-}
--- src/app/trainer/report/page.tsx
+++
@@ -1,4 +0,0 @@
-import { Phase2Disabled } from '@/components/common/Phase2Disabled'
-export default function ReportPage() {
-  return <Phase2Disabled featureName="세션 리포트 작성" />
-}
